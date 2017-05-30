b5c8ffa577a5003cfb0865b4db95f68844d9f314
Zeshan Aslam <zeshan12aslam@gmail.com>
2017-05-30 19:44:35 -0400

    Started index and added logout

--- Site/assets/functions/loginfunctions.php
+++ Site/assets/functions/loginfunctions.php
@@ -6,12 +6,16 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 function login($user, $pass) {
-	$query = "select * from users where username='$user' and password='$pass'";
+	$query = "select * from users where BINARY username='$user' and password='$pass'";
 	$result = mysqli_query($GLOBALS['con'], $query) or die('error');
 	
 	if (mysqli_num_rows($result)) {
 		return true;
 	}
 	return false;
+}
+
+function changePassword($user, $pass) {
+		
 }
 ?>
--- Site/index.php
+++ Site/index.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 
+include 'assets/functions/loginfunctions.php';
+include 'assets/functions/generalfunctions.php';
+
 if (!isset($_SESSION['login'])) {
 	if ($_SESSION['login'] != true) {
 		header("location: login");
@@ -30,91 +33,141 @@ if (!isset($_SESSION['login'])) {
     </head>
 
     <body>
-        <div id="throbber" style="display:none; min-height:120px;"></div>
-        <div id="noty-holder"></div>
-        <div id="wrapper">
-            <!-- Navigation -->
-            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-                    <a class="navbar-brand" href="http://cijulenlinea.ucr.ac.cr/dev-users/">
-                <img src="http://placehold.it/200x50&text=LOGO" alt="LOGO"">
-            </a>
-                </div>
-                <!-- Top Menu Items -->
-                <ul class="nav navbar-right top-nav">
-                    <li><a href="#" data-placement="bottom" data-toggle="tooltip" href="#" data-original-title="Stats"><i class="fa fa-bar-chart-o"></i>
-                </a>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin User <b class="fa fa-angle-down"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#"><i class="fa fa-fw fa-user"></i> Edit Profile</a></li>
-                            <li><a href="#"><i class="fa fa-fw fa-cog"></i> Change Password</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#"><i class="fa fa-fw fa-power-off"></i> Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav side-nav">
-                        <li>
-                            <a href="#" data-toggle="collapse" data-target="#submenu-1"><i class="fa fa-fw fa-search"></i> MENU 1 <i class="fa fa-fw fa-angle-down pull-right"></i></a>
-                            <ul id="submenu-1" class="collapse">
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.1</a></li>
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.2</a></li>
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 1.3</a></li>
+        <div id="navbar-wrapper">
+            <header>
+                <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                            <a class="navbar-brand" href="#">Willow</a>
+                        </div>
+                        <div id="navbar-collapse" class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav navbar-right">
+                                <li class="dropdown">
+                                    <a id="user-profile" href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user-circle fa-lg" aria-hidden="true"></i> <?php echo $_SESSION['username'] ?></a>
+                                    <ul class="dropdown-menu dropdown-block" role="menu">
+                                        <li><a data-toggle="modal" data-target="#changePassword" href="#">Change password</a></li>
+                                        <li><a href="logout.php">Logout</a></li>
+                                    </ul>
+                                </li>
                             </ul>
-                        </li>
-                        <li>
-                            <a href="#" data-toggle="collapse" data-target="#submenu-2"><i class="fa fa-fw fa-star"></i>  MENU 2 <i class="fa fa-fw fa-angle-down pull-right"></i></a>
-                            <ul id="submenu-2" class="collapse">
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.1</a></li>
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.2</a></li>
-                                <li><a href="#"><i class="fa fa-angle-double-right"></i> SUBMENU 2.3</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="investigaciones/favoritas"><i class="fa fa-fw fa-user-plus"></i>  MENU 3</a>
-                        </li>
-                        <li>
-                            <a href="sugerencias"><i class="fa fa-fw fa-paper-plane-o"></i> MENU 4</a>
-                        </li>
-                        <li>
-                            <a href="faq"><i class="fa fa-fw fa fa-question-circle"></i> MENU 5</a>
-                        </li>
-                    </ul>
-                </div>
-                <!-- /.navbar-collapse -->
-            </nav>
-
-            <div id="page-wrapper">
-                <div class="container-fluid">
-                    <!-- Page Heading -->
-                    <div class="row" id="main">
-                        <div class="col-sm-12 col-md-12 well" id="content">
-                            <h1>Welcome Admin!</h1>
                         </div>
                     </div>
-                    <!-- /.row -->
-                </div>
-                <!-- /.container-fluid -->
-            </div>
-            <!-- /#page-wrapper -->
+                </nav>
+            </header>
         </div>
-        <!-- /#wrapper -->
+        <div id="wrapper">
+            <div id="sidebar-wrapper">
+                <aside id="sidebar">
+                    <ul id="sidemenu" class="sidebar-nav">
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-home"></i></span>
+                            <span class="sidebar-title">Home</span>
+                        </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-users"></i></span>
+                            <span class="sidebar-title">Users</span>
+                        </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-file-text"></i></span>
+                            <span class="sidebar-title">Wiki</span>
+                        </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-book"></i></span>
+                            <span class="sidebar-title">Logs</span>
+                        </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-line-chart"></i></span>
+                            <span class="sidebar-title">Stats</span>
+                        </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                            <span class="sidebar-icon"><i class="fa fa-terminal"></i></span>
+                            <span class="sidebar-title">Console</span>
+                        </a>
+                        </li>
+                    </ul>
+                </aside>
+            </div>
+
+            <!-- change pass modal -->
+            <!-- line modal -->
+            <div class="modal fade" id="changePassword" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
+                            <h3 class="modal-title" id="lineModalLabel">Change password</h3>
+                        </div>
+                        <form>
+                            <div class="modal-body">
+
+                                <!-- content goes here -->
+                                <form>
+                                    <div class="form-group">
+                                        <label for="oldpassword">Old password</label>
+                                        <input type="password" class="form-control" name="oldpassword" placeholder="Enter old password">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="newpassword">Current password</label>
+                                        <input type="password" class="form-control" name="newpassword" placeholder="Enter new password">
+                                    </div>
+
+                            </div>
+                            <div class="modal-footer">
+                                <div class="btn-group btn-group-justified" role="group" aria-label="group button">
+                                    <div class="btn-group" role="group">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal" role="button">Close</button>
+                                    </div>
+
+                                    <div class="btn-group" role="group">
+                                        <button type="submit" id="saveImage" class="btn btn-default btn-hover-green" data-action="save" role="button">Change</button>
+                                    </div>
+                                </div>
+                            </div>
+                            </form>
+                    </div>
+                </div>
+            </div>
+
+            <!-- main -->
+            <main id="page-content-wrapper" role="main">
+
+            </main>
+        </div>
 
         <!-- cdn js -->
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="assets/js/notification.js"></script>
+		
+		<?php
+         if (isset($_POST['oldpassword']) && isset($_POST['newpassword'])) {
+			$oldpass = mysqli_real_escape_string($GLOBALS['con'], $_POST['oldpassword']);
+         	$newpass = mysqli_real_escape_string($GLOBALS['con'], $_POST['newpassword']);
+						
+			if (login($_SESSION['username'], $pass)) {
+				
+			} else {
+				//showNoti("error-login");
+			}
+		 }
+		 ?>
     </body>
 
     </html>
