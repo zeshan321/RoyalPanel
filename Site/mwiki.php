c0c81e0944cf14ee8b390b44eb2056fbf026770d
Zeshan Aslam <zeshan12aslam@gmail.com>
2017-06-07 22:08:47 -0400

    Made wiki editor resizable

--- Site/mwiki.php
+++ Site/mwiki.php
@@ -31,7 +31,8 @@ if (!hasPermission("view-wiki")) {
         <!-- cdn css -->
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-
+		<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+		
         <!-- custom css -->
         <link href='assets/css/home.css' rel='stylesheet' type='text/css'>
         <link href='assets/css/users.css' rel='stylesheet' type='text/css'>
@@ -278,7 +279,8 @@ if (!hasPermission("view-wiki")) {
         </div>
 
         <!-- cdn js -->
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="assets/js/notification.js"></script>
         <script type="text/javascript" src="assets/js/editor.js"></script>
@@ -303,6 +305,19 @@ if (!hasPermission("view-wiki")) {
 			$(document).ready(function() {
 				$("#editor").Editor();
 			});
+			
+			$('.modal-content').resizable({
+				  //alsoResize: ".modal-dialog",
+				  minHeight: 300,
+				  minWidth: 300
+				});
+				$('.modal-dialog').draggable();
+
+				$('#editPage').on('show.bs.modal', function() {
+				  $(this).find('.modal-body').css({
+					'max-height': '100%'
+				  });
+				});
 		</script>
 		
 		<?php
